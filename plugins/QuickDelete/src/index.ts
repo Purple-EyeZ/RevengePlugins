ff3f7203b6e2bb2b093c68220aa732808168fb85
Purple_Ξye™ <168314373+Purple-EyeZ@users.noreply.github.com>
2025-01-24 03:53:46 +0100

    more reliable popup type detection

--- plugins/QuickDelete/src/index.ts
+++ plugins/QuickDelete/src/index.ts
@@ -2,6 +2,8 @@ import { findByProps } from "@vendetta/metro";
 import intlProxy from "../intlProxy";
 import { instead } from "@vendetta/patcher";
 
+const autoConfirmKeys = { embed: 'SUPPRESS_EMBED_BODY', message: 'DELETE_MESSAGE_BODY' };
+
 let unpatch;
 
 export default {
@@ -10,7 +12,11 @@ export default {
         if (!Popup) return;
 
         unpatch = instead("show", Popup, (args, fn) => {
-            if ([intlProxy.DELETE_MESSAGE, intlProxy.SUPPRESS_EMBED_TITLE].includes(args?.[0]?.title)) {
+            const titleOrBody = [args?.[0]?.children?.props?.title?.trim(), args?.[0]?.body?.trim()];
+            const isDeletion = (type: keyof typeof autoConfirmKeys) =>
+                titleOrBody.includes(intlProxy[autoConfirmKeys[type]]);
+
+            if (isDeletion('message') || isDeletion('embed')) {
                 args[0].onConfirm?.();
             } else {
                 fn(...args);
