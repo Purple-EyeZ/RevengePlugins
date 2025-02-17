8f81342ee6dea80134872118340f201fbb9746f8
Purple_Ξye™ <168314373+Purple-EyeZ@users.noreply.github.com>
2025-02-17 03:34:57 +0100

    feat: add settings page and remove intlProxy

--- plugins/QuickDelete/src/index.ts
+++ plugins/QuickDelete/src/index.ts
@@ -1,27 +1,50 @@
-import { findByProps } from "@vendetta/metro";
-import intlProxy from "../intlProxy";
-import { instead } from "@vendetta/patcher";
+import { findByProps } from '@vendetta/metro'
+import { instead } from '@vendetta/patcher'
+import { storage } from '@vendetta/plugin'
+import Settings from './Settings'
 
-const autoConfirmKeys = { embed: 'SUPPRESS_EMBED_BODY', message: 'DELETE_MESSAGE_BODY' };
+// intl stuff is based on @nexpid's intlProxy (https://github.com/nexpid/BunnyPlugins/blob/main/src/stuff/lib/intlProxy.ts)
+const { intl, t: intlMap } = findByProps('intl')
 
-let unpatch;
+const KEYS = {
+    message: {
+        hash: 'AMvpS0',
+        storage: 'autoConfirmMessage',
+        default: true,
+    },
+    embed: {
+        hash: 'vXZ+Fh',
+        storage: 'autoConfirmEmbed',
+        default: true,
+    },
+} as const
+
+const autoConfirmMessages = {
+    embed: intl.string(intlMap[KEYS.embed.hash]),
+    message: intl.string(intlMap[KEYS.message.hash]),
+}
+
+let unpatch: () => void
 
 export default {
     onLoad: () => {
-        const Popup = findByProps("show", "openLazy");
-        if (!Popup) return;
+        for (const { storage: key, default: value } of Object.values(KEYS)) {
+            storage[key] ??= value
+        }
 
-        unpatch = instead("show", Popup, (args, fn) => {
-            const titleOrBody = [args?.[0]?.children?.props?.title?.trim(), args?.[0]?.body?.trim()];
-            const isDeletion = (type: keyof typeof autoConfirmKeys) =>
-                titleOrBody.includes(intlProxy[autoConfirmKeys[type]]);
+        const Popup = findByProps('show', 'openLazy')
+        if (!Popup) return
 
-            if (isDeletion('message') || isDeletion('embed')) {
-                args[0].onConfirm?.();
-            } else {
-                fn(...args);
-            }
-        });
+        unpatch = instead('show', Popup, (args, fn) => {
+            const text = [args?.[0]?.children?.props?.title?.trim(), args?.[0]?.body?.trim()]
+
+            const shouldConfirm = (type: 'message' | 'embed') =>
+                storage[KEYS[type].storage] && text.includes(autoConfirmMessages[type])
+
+            return shouldConfirm('message') || shouldConfirm('embed') ? args?.[0]?.onConfirm?.() : fn(...args)
+        })
     },
+
     onUnload: () => unpatch?.(),
-};
+    settings: Settings,
+}
