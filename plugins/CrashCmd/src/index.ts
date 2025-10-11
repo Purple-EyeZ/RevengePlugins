ee666fbf1694d263dce2d34c923e27697327969a
Purple_Ξye™ <168314373+Purple-EyeZ@users.noreply.github.com>
2025-10-11 19:30:40 +0200

    feat: add `Crash Command` plugin

---
+++ plugins/CrashCmd/src/index.ts
@@ -0,0 +1,29 @@
+import { registerCommand } from "@vendetta/commands";
+import { ReactNative } from "@revenge-mod/metro/common";
+
+let command: () => void;
+
+export default {
+    onLoad: () => {
+        command = registerCommand({
+            name: "crash",
+            displayName: "crash",
+            description: "Causes a native client crash.",
+            displayDescription: "Causes a native client crash.",
+            options: [],
+            execute: () => {
+                ReactNative.NativeModules.ExceptionsManager?.reportFatalException(
+                    "Crash requested by user via /crash command.",
+                    [],
+                    0,
+                );
+            },
+            applicationId: "-1",
+            inputType: 1,
+            type: 1,
+        });
+    },
+    onUnload: () => {
+        command?.();
+    },
+};
